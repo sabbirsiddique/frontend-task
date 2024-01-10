399c075f59e11baec28216606ddedf4c24481d69
sabbirsiddique <sabbirsiddique6798@gmail.com>
2024-01-10 22:29:25 +0600

    Incomplete card done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Todo from "./components/Todo";
+import Incomplete from "./components/Incomplete";
 
 function App() {
   return (
@@ -7,41 +7,47 @@ function App() {
       <div>
         <div className="bg-[#FFFFFF] flex gap-4">
           <div className="bg-[#F2F4F7]">
-            <div className="flex p-5 items-center">
-              <div className="bg-red-600 border-1 h-5 w-5 rounded-l-lg"></div>
-              <div>
-                <p className="ml-2">Incomplete</p>
+            <div className="flex p-5 justify-between">
+              <div className="flex justify-between items-center">
+                <div className="bg-red-600 border-1 h-5 w-5 rounded-l-lg"></div>
+                <div>
+                  <p className="ml-2">Incomplete</p>
+                </div>
               </div>
               <div className="ml-[100px]">
                 <p className="bg-blue-50 p-2">0</p>
               </div>
             </div>
-            <Todo></Todo>
+            <Incomplete></Incomplete>
           </div>
           <div className="bg-[#F2F4F7]">
-            <div className="flex p-5 items-center">
+            <div className="flex p-5 justify-between">
+              <div className="flex justify-between items-center">
               <div className="bg-blue-400 border-1 h-5 w-5 rounded-l-lg"></div>
               <div>
                 <p className="ml-2">To Do</p>
               </div>
+              </div>
               <div className="ml-[100px]">
                 <p className="bg-blue-50 p-2">0</p>
               </div>
             </div>
           </div>
           <div className="bg-[#F2F4F7]">
-            <div className="flex p-5 items-center">
+            <div className="flex p-5 justify-between">
+              <div className="flex justify-between items-center">
               <div className="bg-yellow-400 border-1 h-5 w-5 rounded-l-lg"></div>
               <div>
                 <p className="ml-2">Doing</p>
               </div>
+              </div>
               <div className="ml-[100px]">
                 <p className="bg-blue-50 p-2">0</p>
               </div>
             </div>
           </div>
           <div className="bg-[#F2F4F7]">
-            <div className="flex p-5 items-center">
+            <div className="flex p-5 justify-between items-center">
               <div>
                 <p className="ml-2">Under Review</p>
               </div>
@@ -51,7 +57,7 @@ function App() {
             </div>
           </div>
           <div className="bg-[#F2F4F7]">
-            <div className="flex p-5 items-center">
+            <div className="flex p-5 justify-between items-center">
               <div>
                 <p className="ml-2">Completed</p>
               </div>
@@ -61,7 +67,7 @@ function App() {
             </div>
           </div>
           <div className="bg-[#F2F4F7]">
-            <div className="flex p-5 items-center">
+            <div className="flex p-5 justify-between items-center">
               <div>
                 <p className="ml-2">Overdoing</p>
               </div>
---
+++ src/components/Incomplete.jsx
@@ -0,0 +1,332 @@
+import { FaLayerGroup } from "react-icons/fa6";
+import { FaClipboardList } from "react-icons/fa";
+import { ImAttachment } from "react-icons/im";
+import { FaRegComments } from "react-icons/fa";
+import { FaCalendarAlt } from "react-icons/fa";
+
+const Incomplete = () => {
+  return (
+    <div>
+      <div className="bg-white m-2">
+      <div className="flex justify-between p-2">
+        <div className="flex items-center">
+          <img
+            src="https://i.ibb.co/jZN85mT/sabbir.png"
+            className="h-10 w-10 rounded-full mr-1"
+          />
+          <p className="font-bold">Client Names</p>
+        </div>
+        <div className="flex items-center">
+          <img
+            src="https://i.ibb.co/16Sc347/deliveryman4.png"
+            className="h-10 w-10 rounded-full mr-1"
+          />
+          <p className="font-bold">Sadik Istiak</p>
+        </div>
+      </div>
+      <div className="flex">
+        <div className="flex items-center p-1 gap-1 ml-2">
+          <FaLayerGroup></FaLayerGroup>
+          <p className="mr-5">Lorem ipsum dolor sit amet consectetur.</p>
+        </div>
+        <div className="flex items-center p-2">
+          <FaClipboardList></FaClipboardList>
+          <p>1/2</p>
+        </div>
+      </div>
+      <div className="flex justify-between p-2 mt-2">
+        <img
+          src="https://i.ibb.co/SKFT6mS/deliveryman2.png"
+          className="h-10 w-10 rounded-full"
+        />
+        <img
+          src="https://i.ibb.co/HHBGwpD/deliveryman3.png"
+          className="h-10 w-10 rounded-full"
+        />
+        <div className="h-10 w-10 rounded-full bg-blue-50 flex items-center">
+            <p className="mx-auto">12+</p>
+            </div>
+        <div className="flex items-center gap-2">
+          <FaRegComments></FaRegComments>
+          <p>15</p>
+        </div>
+        <div className="flex items-center gap-2">
+          <ImAttachment></ImAttachment>
+          <p>0</p>
+        </div>
+        <div className="flex items-center gap-2">
+            <FaCalendarAlt></FaCalendarAlt>
+            <p>01</p>
+        </div>
+      </div>
+      </div>
+      <div className="bg-white m-2">
+      <div className="flex justify-between p-2">
+        <div className="flex items-center">
+          <img
+            src="https://i.ibb.co/jZN85mT/sabbir.png"
+            className="h-10 w-10 rounded-full mr-1"
+          />
+          <p className="font-bold">Client Names</p>
+        </div>
+        <div className="flex items-center">
+          <img
+            src="https://i.ibb.co/16Sc347/deliveryman4.png"
+            className="h-10 w-10 rounded-full mr-1"
+          />
+          <p className="font-bold">Sadik Istiak</p>
+        </div>
+      </div>
+      <div className="flex">
+        <div className="flex items-center p-1 gap-1 ml-2">
+          <FaLayerGroup></FaLayerGroup>
+          <p className="mr-5">Lorem ipsum dolor sit amet consectetur.</p>
+        </div>
+        <div className="flex items-center p-2">
+          <FaClipboardList></FaClipboardList>
+          <p>1/2</p>
+        </div>
+      </div>
+      <div className="flex justify-between p-2 mt-2">
+        <img
+          src="https://i.ibb.co/SKFT6mS/deliveryman2.png"
+          className="h-10 w-10 rounded-full"
+        />
+        <img
+          src="https://i.ibb.co/HHBGwpD/deliveryman3.png"
+          className="h-10 w-10 rounded-full"
+        />
+        <div className="h-10 w-10 rounded-full bg-blue-50 flex items-center">
+            <p className="mx-auto">12+</p>
+            </div>
+        <div className="flex items-center gap-2">
+          <FaRegComments></FaRegComments>
+          <p>15</p>
+        </div>
+        <div className="flex items-center gap-2">
+          <ImAttachment></ImAttachment>
+          <p>0</p>
+        </div>
+        <div className="flex items-center gap-2">
+            <FaCalendarAlt></FaCalendarAlt>
+            <p>01</p>
+        </div>
+      </div>
+      </div>
+      <div className="bg-white m-2">
+      <div className="flex justify-between p-2">
+        <div className="flex items-center">
+          <img
+            src="https://i.ibb.co/jZN85mT/sabbir.png"
+            className="h-10 w-10 rounded-full mr-1"
+          />
+          <p className="font-bold">Client Names</p>
+        </div>
+        <div className="flex items-center">
+          <img
+            src="https://i.ibb.co/16Sc347/deliveryman4.png"
+            className="h-10 w-10 rounded-full mr-1"
+          />
+          <p className="font-bold">Sadik Istiak</p>
+        </div>
+      </div>
+      <div className="flex">
+        <div className="flex items-center p-1 gap-1 ml-2">
+          <FaLayerGroup></FaLayerGroup>
+          <p className="mr-5">Lorem ipsum dolor sit amet consectetur.</p>
+        </div>
+        <div className="flex items-center p-2">
+          <FaClipboardList></FaClipboardList>
+          <p>1/2</p>
+        </div>
+      </div>
+      <div className="flex justify-between p-2 mt-2">
+        <img
+          src="https://i.ibb.co/SKFT6mS/deliveryman2.png"
+          className="h-10 w-10 rounded-full"
+        />
+        <img
+          src="https://i.ibb.co/HHBGwpD/deliveryman3.png"
+          className="h-10 w-10 rounded-full"
+        />
+        <div className="h-10 w-10 rounded-full bg-blue-50 flex items-center">
+            <p className="mx-auto">12+</p>
+            </div>
+        <div className="flex items-center gap-2">
+          <FaRegComments></FaRegComments>
+          <p>15</p>
+        </div>
+        <div className="flex items-center gap-2">
+          <ImAttachment></ImAttachment>
+          <p>0</p>
+        </div>
+        <div className="flex items-center gap-2">
+            <FaCalendarAlt></FaCalendarAlt>
+            <p>01</p>
+        </div>
+      </div>
+      </div>
+      <div className="bg-white m-2">
+      <div className="flex justify-between p-2">
+        <div className="flex items-center">
+          <img
+            src="https://i.ibb.co/jZN85mT/sabbir.png"
+            className="h-10 w-10 rounded-full mr-1"
+          />
+          <p className="font-bold">Client Names</p>
+        </div>
+        <div className="flex items-center">
+          <img
+            src="https://i.ibb.co/16Sc347/deliveryman4.png"
+            className="h-10 w-10 rounded-full mr-1"
+          />
+          <p className="font-bold">Sadik Istiak</p>
+        </div>
+      </div>
+      <div className="flex">
+        <div className="flex items-center p-1 gap-1 ml-2">
+          <FaLayerGroup></FaLayerGroup>
+          <p className="mr-5">Lorem ipsum dolor sit amet consectetur.</p>
+        </div>
+        <div className="flex items-center p-2">
+          <FaClipboardList></FaClipboardList>
+          <p>1/2</p>
+        </div>
+      </div>
+      <div className="flex justify-between p-2 mt-2">
+        <img
+          src="https://i.ibb.co/SKFT6mS/deliveryman2.png"
+          className="h-10 w-10 rounded-full"
+        />
+        <img
+          src="https://i.ibb.co/HHBGwpD/deliveryman3.png"
+          className="h-10 w-10 rounded-full"
+        />
+        <div className="h-10 w-10 rounded-full bg-blue-50 flex items-center">
+            <p className="mx-auto">12+</p>
+            </div>
+        <div className="flex items-center gap-2">
+          <FaRegComments></FaRegComments>
+          <p>15</p>
+        </div>
+        <div className="flex items-center gap-2">
+          <ImAttachment></ImAttachment>
+          <p>0</p>
+        </div>
+        <div className="flex items-center gap-2">
+            <FaCalendarAlt></FaCalendarAlt>
+            <p>01</p>
+        </div>
+      </div>
+      </div>
+      <div className="bg-white m-2">
+      <div className="flex justify-between p-2">
+        <div className="flex items-center">
+          <img
+            src="https://i.ibb.co/jZN85mT/sabbir.png"
+            className="h-10 w-10 rounded-full mr-1"
+          />
+          <p className="font-bold">Client Names</p>
+        </div>
+        <div className="flex items-center">
+          <img
+            src="https://i.ibb.co/16Sc347/deliveryman4.png"
+            className="h-10 w-10 rounded-full mr-1"
+          />
+          <p className="font-bold">Sadik Istiak</p>
+        </div>
+      </div>
+      <div className="flex">
+        <div className="flex items-center p-1 gap-1 ml-2">
+          <FaLayerGroup></FaLayerGroup>
+          <p className="mr-5">Lorem ipsum dolor sit amet consectetur.</p>
+        </div>
+        <div className="flex items-center p-2">
+          <FaClipboardList></FaClipboardList>
+          <p>1/2</p>
+        </div>
+      </div>
+      <div className="flex justify-between p-2 mt-2">
+        <img
+          src="https://i.ibb.co/SKFT6mS/deliveryman2.png"
+          className="h-10 w-10 rounded-full"
+        />
+        <img
+          src="https://i.ibb.co/HHBGwpD/deliveryman3.png"
+          className="h-10 w-10 rounded-full"
+        />
+        <div className="h-10 w-10 rounded-full bg-blue-50 flex items-center">
+            <p className="mx-auto">12+</p>
+            </div>
+        <div className="flex items-center gap-2">
+          <FaRegComments></FaRegComments>
+          <p>15</p>
+        </div>
+        <div className="flex items-center gap-2">
+          <ImAttachment></ImAttachment>
+          <p>0</p>
+        </div>
+        <div className="flex items-center gap-2">
+            <FaCalendarAlt></FaCalendarAlt>
+            <p>01</p>
+        </div>
+      </div>
+      </div>
+      <div className="bg-white m-2">
+      <div className="flex justify-between p-2">
+        <div className="flex items-center">
+          <img
+            src="https://i.ibb.co/jZN85mT/sabbir.png"
+            className="h-10 w-10 rounded-full mr-1"
+          />
+          <p className="font-bold">Client Names</p>
+        </div>
+        <div className="flex items-center">
+          <img
+            src="https://i.ibb.co/16Sc347/deliveryman4.png"
+            className="h-10 w-10 rounded-full mr-1"
+          />
+          <p className="font-bold">Sadik Istiak</p>
+        </div>
+      </div>
+      <div className="flex">
+        <div className="flex items-center p-1 gap-1 ml-2">
+          <FaLayerGroup></FaLayerGroup>
+          <p className="mr-5">Lorem ipsum dolor sit amet consectetur.</p>
+        </div>
+        <div className="flex items-center p-2">
+          <FaClipboardList></FaClipboardList>
+          <p>1/2</p>
+        </div>
+      </div>
+      <div className="flex justify-between p-2 mt-2">
+        <img
+          src="https://i.ibb.co/SKFT6mS/deliveryman2.png"
+          className="h-10 w-10 rounded-full"
+        />
+        <img
+          src="https://i.ibb.co/HHBGwpD/deliveryman3.png"
+          className="h-10 w-10 rounded-full"
+        />
+        <div className="h-10 w-10 rounded-full bg-blue-50 flex items-center">
+            <p className="mx-auto">12+</p>
+            </div>
+        <div className="flex items-center gap-2">
+          <FaRegComments></FaRegComments>
+          <p>15</p>
+        </div>
+        <div className="flex items-center gap-2">
+          <ImAttachment></ImAttachment>
+          <p>0</p>
+        </div>
+        <div className="flex items-center gap-2">
+            <FaCalendarAlt></FaCalendarAlt>
+            <p>01</p>
+        </div>
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default Incomplete;
